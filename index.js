cdf8f671133da7107e6021252678449182ef7dbf
AnmolSinghNegi1 <144584412+AnmolSinghNegi1@users.noreply.github.com>
2024-06-09 13:58:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const cors= require("cors");
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: 'https://tutorfinderfrontend.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods if necessary
+    credentials: true // If you need to include cookies or authentication headers
+}));
 require("./config/database").connect()
 
 // route import and mount 
